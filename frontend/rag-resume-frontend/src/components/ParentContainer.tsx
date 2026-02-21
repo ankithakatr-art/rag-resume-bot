2e6d30868225f9a5459db7aea6fc3a9a3b533516
ankithakatr-art <ankitha.katr@gmail.com>
2026-02-21 10:59:52 +0530

    Fixed css #3

--- frontend/rag-resume-frontend/src/components/ParentContainer.tsx
+++ frontend/rag-resume-frontend/src/components/ParentContainer.tsx
@@ -3,7 +3,7 @@ import { TypeAnimation } from 'react-type-animation';
 import TextContainer from "./TextContainer";
 
 export default function ParentContainer() {
-    return (<div>
+    return (<div style={{ textAlign: 'center', width: '100%' }}>
         <TypeAnimation className="heading-container" sequence={[' Hello, welcome to learning more about Ankitha!', 1000, 'Type some questions below to know more!', 10000]}
             wrapper='span' speed={10}
             style={{ fontSize: '2em', display: 'inline-block' }}
